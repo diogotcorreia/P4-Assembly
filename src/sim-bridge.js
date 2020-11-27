eb6a7fc8c1ebb236d930a5fe8240dd31cd2ceecc
Diogo Correia <me@diogotc.com>
2020-11-27 23:52:03 +0000

    Add button to toggle memory visibility
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- src/sim-bridge.js
+++ src/sim-bridge.js
@@ -4,6 +4,15 @@ const previousState = vscode.getState();
 if (previousState && previousState.content)
   window.codeMirror.setValue(previousState.content);
 
+const btn = document.createElement("button");
+btn.innerText = "Toggle Memory";
+btn.addEventListener("click", () => {
+  const x = document.getElementById("simMemory");
+  if (x.style.display === "none") x.style.display = "inline-flex";
+  else x.style.display = "none";
+});
+document.getElementById("simButtons").appendChild(btn);
+
 window.addEventListener("message", (event) => {
   const message = event.data; // The JSON data our extension sent
 
